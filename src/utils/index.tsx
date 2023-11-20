c3b6f3c222a1e92e0420bbba7ab7b120d062f524
tattoo8436 <94815257+tattoo8436@users.noreply.github.com>
2023-11-20 15:54:09 +0700

    deploy

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,4 +2,4 @@ export const getCurrentAccount: any = () => {
   return JSON.parse(localStorage.getItem("account") ?? "{}");
 };
 
-export const BASE_URL_API = "http://localhost:8080/api";
+export const BASE_URL_API = "https://imdb-service-production.up.railway.app/api";
